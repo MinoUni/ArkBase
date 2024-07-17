cea589b3626d59f0db210bdbbf8a01fc75316630
MinoUni <maks.reva.ua@gmail.com>
2024-07-17 07:42:10 +0300

    project init

---
+++ src/test/java/com/arkbase/ArkbaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.arkbase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArkbaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
