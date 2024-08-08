def40a5b6e7d591efbfcf64cd17a8c47c93786b3
MinoUni <maks.reva.ua@gmail.com>
2024-08-08 15:50:34 +0300

    add rarity exception handler

--- src/main/java/com/arkbase/handler/CustomExceptionHandler.java
+++ src/main/java/com/arkbase/handler/CustomExceptionHandler.java
@@ -2,6 +2,7 @@ package com.arkbase.handler;
 
 import com.arkbase.exception.OperatorAlreadyExistsException;
 import com.arkbase.exception.OperatorNotFoundException;
+import com.arkbase.exception.OperatorRarityException;
 import java.util.List;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
@@ -24,7 +25,8 @@ public class CustomExceptionHandler extends ResponseEntityExceptionHandler {
       @NonNull HttpHeaders headers,
       @NonNull HttpStatusCode status,
       @NonNull WebRequest request) {
-    ApiError apiError = new ApiError(HttpStatus.valueOf(status.value()), "Validation failed");
+    HttpStatus httpStatus = HttpStatus.valueOf(status.value());
+    ApiError apiError = new ApiError(httpStatus, "Validation failed");
     List<SubError> subErrors = apiError.getSubErrors();
     e.getBindingResult()
         .getFieldErrors()
@@ -38,9 +40,7 @@ public class CustomExceptionHandler extends ResponseEntityExceptionHandler {
                       error.getDefaultMessage());
               subErrors.add(subError);
             });
-    return ResponseEntity.status(status)
-        .contentType(MediaType.APPLICATION_PROBLEM_JSON)
-        .body(apiError);
+    return buildResponseEntity(httpStatus, apiError);
   }
 
   @ExceptionHandler(OperatorAlreadyExistsException.class)
@@ -48,17 +48,26 @@ public class CustomExceptionHandler extends ResponseEntityExceptionHandler {
       OperatorAlreadyExistsException e) {
     HttpStatus status = HttpStatus.CONFLICT;
     ApiError apiError = new ApiError(status, e.getMessage());
-    return ResponseEntity.status(status)
-        .contentType(MediaType.APPLICATION_PROBLEM_JSON)
-        .body(apiError);
+    return buildResponseEntity(status, apiError);
   }
 
   @ExceptionHandler(OperatorNotFoundException.class)
   public ResponseEntity<Object> handleOperatorNotFoundException(OperatorNotFoundException e) {
     HttpStatus status = HttpStatus.NOT_FOUND;
     ApiError apiError = new ApiError(status, e.getMessage());
+    return buildResponseEntity(status, apiError);
+  }
+
+  @ExceptionHandler(OperatorRarityException.class)
+  public ResponseEntity<Object> handleOperatorRarityException(OperatorRarityException e) {
+    HttpStatus status = HttpStatus.CONFLICT;
+    ApiError apiError = new ApiError(status, e.getMessage());
+    return buildResponseEntity(status, apiError);
+  }
+
+  private ResponseEntity<Object> buildResponseEntity(HttpStatus status, ApiError content) {
     return ResponseEntity.status(status)
         .contentType(MediaType.APPLICATION_PROBLEM_JSON)
-        .body(apiError);
+        .body(content);
   }
 }
