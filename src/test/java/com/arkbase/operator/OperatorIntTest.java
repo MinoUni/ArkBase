9b690177a5bed131ad5400728153d510c110998e
MinoUni <maks.reva.ua@gmail.com>
2024-08-12 15:28:55 +0300

    Add OperatorController integration tests

--- src/test/java/com/arkbase/operator/OperatorIntTest.java
+++ src/test/java/com/arkbase/operator/OperatorIntTest.java
@@ -1,23 +1,19 @@
 package com.arkbase.operator;
 
-import static org.hamcrest.Matchers.hasSize;
-import static org.hamcrest.Matchers.is;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.patch;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.header;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-import com.arkbase.enums.Rarity;
-import com.arkbase.operator.enums.Archetype;
-import com.arkbase.operator.enums.Subclass;
-import com.arkbase.operator.enums.Trait;
 import com.arkbase.utils.TestUtils;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import org.hamcrest.Matchers;
+import org.junit.jupiter.api.MethodOrderer;
 import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -25,42 +21,85 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @AutoConfigureMockMvc
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class OperatorIntTest {
 
-  @Autowired private MockMvc mvc;
-
-  @Autowired private ObjectMapper jsonMapper;
+  @Autowired private MockMvc mockMvc;
 
   @Test
   @Order(1)
-  void shouldAddOperator() throws Exception {
-    String newOperator = TestUtils.readWholeFile("add-operator-request.json");
-    mvc.perform(post("/operators").contentType(MediaType.APPLICATION_JSON).content(newOperator))
+  void shouldCreateOperator() throws Exception {
+    String req = TestUtils.readWholeFile("add-operator-request.json");
+    String resp = TestUtils.readWholeFile("add-operator-response.json");
+
+    mockMvc
+        .perform(post("/operators").contentType(MediaType.APPLICATION_JSON).content(req))
         .andExpectAll(
             status().isCreated(),
             header().exists(HttpHeaders.LOCATION),
             header().string(HttpHeaders.LOCATION, Matchers.containsString("/operators/1")),
-            jsonPath("$.id", is(1)),
-            jsonPath("$.codeName", is("Ray")),
-            jsonPath("$.archetype", is(Archetype.SNIPER.getArchetype())),
-            jsonPath("$.subclass", is(Subclass.HUNTER.getSubclass())),
-            jsonPath("$.rarity", is(Rarity.SIX_STAR.getRarity())),
-            jsonPath("$.trait", is(Trait.HUNTER.getTrait())),
-            jsonPath("$.skills").isArray(),
-            jsonPath("$.skills", hasSize(2)),
-            jsonPath("$._links").exists());
+            content().json(resp));
   }
 
   @Test
   @Order(2)
+  void shouldFindOperatorById() throws Exception {
+    int operatorId = 1;
+    String resp = TestUtils.readWholeFile("find-operator-by-id-response.json");
+
+    mockMvc
+        .perform(get(String.format("/operators/%d", operatorId)))
+        .andExpectAll(
+            status().isOk(),
+            content().contentType(MediaType.APPLICATION_JSON),
+            content().json(resp));
+  }
+
+  @Test
+  @Order(3)
+  void shouldAddSkillToOperator() throws Exception {
+    int operatorId = 1;
+    String req = TestUtils.readWholeFile("add-skill-to-operator-request.json");
+    String resp = TestUtils.readWholeFile("add-skill-to-operator-response.json");
+
+    mockMvc
+        .perform(
+            post(String.format("/operators/%d/skills", operatorId))
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(req))
+        .andExpectAll(
+            status().isCreated(),
+            content().contentType(MediaType.APPLICATION_JSON),
+            content().json(resp));
+  }
+
+  @Test
+  @Order(4)
+  void shouldRemoveSkillFromOperator() throws Exception {
+    int operatorId = 1, skillId = 2;
+    String resp = TestUtils.readWholeFile("remove-skill-from-operator-response.json");
+
+    mockMvc
+        .perform(
+            delete(String.format("/operators/%d/skills", operatorId))
+                .param("skillId", String.valueOf(skillId)))
+        .andExpectAll(
+            status().isOk(),
+            content().contentType(MediaType.APPLICATION_JSON),
+            content().json(resp));
+  }
+
+  @Test
+  @Order(5)
   void shouldUpdateOperatorDetails() throws Exception {
     int id = 1;
     String req = TestUtils.readWholeFile("update-operator-details-request.json");
     String resp = TestUtils.readWholeFile("update-operator-details-response.json");
 
-    mvc.perform(
+    mockMvc
+        .perform(
             patch(String.format("/operators/%d", id))
                 .contentType(MediaType.APPLICATION_JSON)
                 .content(req))
